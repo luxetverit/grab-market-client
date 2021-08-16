d8ef07c69367f21505a5d209256849ac8d0548c4
jwnam <replaysoccer@naver.com>
2021-08-16 23:11:56 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,46 @@
-import { useParams, userParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, userState, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://01350041-ed6a-4295-ac30-775d05994da8.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="content-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2021년 8월 15일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
